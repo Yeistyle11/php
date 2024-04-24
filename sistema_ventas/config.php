c18f01632426e6012688f63e9918c42ff99f4591
yeistyle11 <yeison.gar074@gmail.com>
2024-04-24 12:59:37 -0500

    cambios

---
+++ sistema_ventas/config.php
@@ -0,0 +1,21 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//Iniciamos la session
+session_start();
+date_default_timezone_set("America/Bogota");
+
+
+class Config {
+    const BBDD_HOST = "127.0.0.1"; //127.0.0.1
+    const BBDD_PORT = "3306"; //3306
+    const BBDD_USUARIO = "root"; //root
+    const BBDD_CLAVE = "YeisonGarcia1109";
+    const BBDD_NOMBRE = "abmventas";
+
+}
+
+
+?>
